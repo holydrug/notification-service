43e52f1084c2e9d4fc5fc835a5fcba00e9686439
holydrug <nonprofitxz@gmail.com>
2022-05-28 17:38:15 +0300

    added query to select filtered PhoneNumber from db with carrier code

--- src/main/java/com/popov/notification/service/repository/person/repository/phone/PhoneNumberRepository.java
+++ src/main/java/com/popov/notification/service/repository/person/repository/phone/PhoneNumberRepository.java
@@ -2,6 +2,13 @@ package com.popov.notification.service.repository.person.repository.phone;
 
 import com.popov.notification.service.entity.person.phone.PhoneNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PhoneNumberRepository extends JpaRepository<PhoneNumber, Long> {
+
+    @Query(value = "SELECT * FROM phones WHERE carrier_code=:carrierCode", nativeQuery = true)
+    List<PhoneNumber> getFilteredPhonesWithCarrierCode(@Param("carrierCode") String carrierCode);
 }
