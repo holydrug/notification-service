a93ff044e811431cf716c82cbd9194ad03f0a0b1
holydrug <nonprofitxz@gmail.com>
2022-05-28 17:44:33 +0300

    minor changes in test

--- src/test/java/com/popov/notification/service/NotificationServiceApplicationTests.java
+++ src/test/java/com/popov/notification/service/NotificationServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.popov.notification.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotificationServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
