d3abce91297afacee928fa01d968100b2848cece
holydrug <nonprofitxz@gmail.com>
2022-05-28 17:40:51 +0300

    minor changes in imports, lombok annotation and etc

--- src/main/java/com/popov/notification/service/configuration/PersonConfiguration.java
+++ src/main/java/com/popov/notification/service/configuration/PersonConfiguration.java
@@ -1,18 +1,23 @@
-//package com.popov.notification.service.configuration;
-//
-//import com.popov.notification.service.entity.person.Person;
-//import com.popov.notification.service.entity.person.phone.PhoneNumber;
-//import com.popov.notification.service.repository.person.repository.PersonRepository;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class PersonConfiguration {
-//
-//    @Bean
-//    public CommandLineRunner saveSomeClients(PersonRepository personRepository) {
-//
-//        return args -> personRepository.save(new Person("AMOGUS", new PhoneNumber("RU", "7977a5798544")));
-//    }
-//}
+package com.popov.notification.service.configuration;
+
+import com.popov.notification.service.entity.person.Person;
+import com.popov.notification.service.entity.person.phone.PhoneNumber;
+import com.popov.notification.service.repository.person.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersonConfiguration {
+
+    @Bean
+    public CommandLineRunner saveSomeClients(PersonRepository personRepository) {
+        return args -> {
+            personRepository.save(new Person("SIMEN", new PhoneNumber("RU", "79262675371"), "notifiedservice@gmail.com"));
+            personRepository.save(new Person("AMOGUS", new PhoneNumber("RU", "79775798542"), "notifiedservice@gmail.com"));
+            personRepository.save(new Person("SEBILLA", new PhoneNumber("RU", "79685243033"), "notifiedservice@gmail.com"));
+            personRepository.save(new Person("SELICIA", new PhoneNumber("RU", "79117762654"), "notifiedservice@gmail.com"));
+            personRepository.save(new Person("AMIRO", new PhoneNumber("RU", "79670503235"), "notifiedservice@gmail.com"));
+        };
+    }
+}
--- src/main/java/com/popov/notification/service/controllers/people/controller/PeopleController.java
+++ src/main/java/com/popov/notification/service/controllers/people/controller/PeopleController.java
@@ -44,4 +44,5 @@ public class PeopleController {
         personService.delete(id);
     }
 
+
 }
