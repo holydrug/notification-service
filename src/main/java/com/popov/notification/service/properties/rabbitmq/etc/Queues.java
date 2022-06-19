6e4c07a5a65538e8ca8d72fdc5fcbb80eba73df0
holydrug <nonprofitxz@gmail.com>
2022-06-19 20:18:02 +0300

    updated yaml config

---
+++ src/main/java/com/popov/notification/service/properties/rabbitmq/etc/Queues.java
@@ -0,0 +1,10 @@
+package com.popov.notification.service.properties.rabbitmq.etc;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Queues {
+    private String notification;
+}
