dba8a6a20adc5a5276acc85731b4ec6980e657b2
holydrug <nonprofitxz@gmail.com>
2022-06-19 20:19:24 +0300

    updated yaml config class

--- src/main/java/com/popov/notification/service/properties/rabbitmq/RabbitMq.java
+++ src/main/java/com/popov/notification/service/properties/rabbitmq/RabbitMq.java
@@ -1,13 +1,21 @@
 package com.popov.notification.service.properties.rabbitmq;
 
+import com.popov.notification.service.properties.rabbitmq.etc.Exchanges;
+import com.popov.notification.service.properties.rabbitmq.etc.Queues;
+import com.popov.notification.service.properties.rabbitmq.etc.RoutingKeys;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RabbitMq {
+
+    private int port;
     private String host;
     private String username;
     private String password;
-    private int port;
+
+    private Exchanges exchanges;
+    private Queues queues;
+    private RoutingKeys routingKeys;
 }
