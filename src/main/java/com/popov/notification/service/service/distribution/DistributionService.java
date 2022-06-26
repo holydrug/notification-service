f60c70a36db38191d805c3b7e968f619efaced3c
holydrug <nonprofitxz@gmail.com>
2022-06-26 13:16:36 +0300

    updated project (added interface for each service)

--- src/main/java/com/popov/notification/service/service/distribution/DistributionService.java
+++ src/main/java/com/popov/notification/service/service/distribution/DistributionService.java
@@ -1,23 +1,8 @@
 package com.popov.notification.service.service.distribution;
 
 import com.popov.notification.service.entity.mail.dto.MailDto;
-import com.popov.notification.service.service.distribution.filter.DistributionFilterService;
-import com.popov.notification.service.service.mail.MailService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class DistributionService {
-
-    private final MailService mailService;
-    private final DistributionFilterService distrFilterService;
-
-    public void sendMailsToAllEmail(MailDto mailDto) {
-        distrFilterService.getListOfAllMails(mailDto).forEach(mailService::send);
-    }
-
-    public void sendFilteredMailsWithCarrierCode(MailDto mailDto, String carrierCode) {
-        distrFilterService.getFilteredMailsWithCarrierCode(mailDto, carrierCode).forEach(mailService::send);
-    }
+public interface DistributionService {
+    void sendMailsToAllEmail(MailDto mailDto);
+    void sendFilteredMailsWithCarrierCode(MailDto mailDto, String carrierCode);
 }
